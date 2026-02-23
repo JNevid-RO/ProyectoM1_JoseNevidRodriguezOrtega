fb32535b5e78de057a0690aa41410612dd43b43a
Jose Nevid Ortega <nevidrodriguez01@gmail.com>
2026-02-23 12:58:29 -0600

    feat: Logica necesaria para convertir el codigo de color HSL a HEX

--- JS/script.js
+++ JS/script.js
@@ -4,3 +4,29 @@ function generarColorHSL() {
   const l = Math.floor(Math.random() * 70) + 20;
   return `hsl(${h}, ${s}%, ${l}%)`;
 }
+
+function convertirHSL_A_HEX(hsl) {
+  const [h, s, l] = hsl.match(/\d+/g).map(Number);
+  const sN = s / 100;
+  const lN = l / 100;
+
+  const c = (1 - Math.abs(2 * lN - 1)) * sN;
+  const x = c * (1 - Math.abs((h / 60) % 2 - 1));
+  const m = lN - c / 2;
+
+  let r = 0, g = 0, b = 0;
+
+  if (h < 60) [r,g,b] = [c,x,0];
+  else if (h < 120) [r,g,b] = [x,c,0];
+  else if (h < 180) [r,g,b] = [0,c,x];
+  else if (h < 240) [r,g,b] = [0,x,c];
+  else if (h < 300) [r,g,b] = [x,0,c];
+  else [r,g,b] = [c,0,x];
+
+  const aHex = v => {
+    const hex = Math.round((v+m)*255).toString(16);
+    return hex.length === 1 ? "0" + hex : hex;
+  };
+
+  return `#${aHex(r)}${aHex(g)}${aHex(b)}`;
+}
